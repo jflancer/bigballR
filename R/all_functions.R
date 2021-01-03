36248b5e8e549c2f9cf36a38348a88ea8daf6a4b
Jake Flancer <jake@Jakes-MacBook-Pro.local>
2021-01-03 14:37:00 -0500

    version detect fix

--- R/all_functions.R
+++ R/all_functions.R
@@ -116,7 +116,8 @@ scrape_game <- function(game_id, save_file=F, use_file=F, base_path = NA, overwr
   format <-
     if (((first_half[1, 1] == "20:00:00") &
         (first_half[1, 2] == "game start" |
-         first_half[1, 2] == "period start")) |
+         first_half[1, 2] == "period start"|
+         first_half[1,2] == "jumpball startperiod")) |
         any(grepl("commercial",game[,2]))) {
       "V2"
     } else{
